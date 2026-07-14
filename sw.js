16a0e723a5176de09df896d2584d5c2e2fb72051
fryjin <frychengeng@gmail.com>
2026-07-14 16:53:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dreamland-pwa-v23';
+const CACHE_VERSION = 'dreamland-pwa-v24';
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
