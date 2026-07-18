7948c3fe8b9ac25c8db457c6bb3cacd0fedec7f1
fryjin <frychengeng@gmail.com>
2026-07-18 14:14:16 +0800

    Update cache version to 'dreamland-pwa-v39'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dreamland-pwa-v38';
+const CACHE_VERSION = 'dreamland-pwa-v39';
 
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
