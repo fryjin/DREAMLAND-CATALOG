7bd562692d904fdeae6ba70f7ce0c51b0578cebb
fryjin <frychengeng@gmail.com>
2026-07-22 17:46:56 +0800

    Update cache version to v46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dreamland-pwa-v45';
+const CACHE_VERSION = 'dreamland-pwa-v46';
 
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
