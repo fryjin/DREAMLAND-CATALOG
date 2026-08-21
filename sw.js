521d5bbf8980a0fc291f5b701bef4036529a4ac0
fryjin <278090937+fryjin@users.noreply.github.com>
2026-08-21 10:15:30 +0000

    B6-04.1: align product data with repository assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'dreamland-pwa-v81';
+const CACHE_VERSION = 'dreamland-pwa-v82';
 
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
